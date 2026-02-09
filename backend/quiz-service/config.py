2e7b2e968e820baa2d873dbb91a7198a3302c38a
12nikola <nikola.rakita98@gmail.com>
2026-02-09 23:25:58 +0100

    config
    config

--- backend/quiz-service/config.py
+++ backend/quiz-service/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://postgres:user@localhost:5432/quiz_users")
-    REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MONGO_URI = os.environ.get("MONGO_URI", "mongodb://admin:admin@localhost:27017/quiz_db?authSource=admin")
+    MONGO_DB = os.environ.get("MONGO_DB", "quiz_db")
+    MAIN_SERVICE_URL = os.environ.get("MAIN_SERVICE_URL", "http://localhost:5000")
