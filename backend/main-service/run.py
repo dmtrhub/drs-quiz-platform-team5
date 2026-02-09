e4eb3df152cab29b6d1709596ab78f9145a05ea3
12nikola <nikola.rakita98@gmail.com>
2026-02-10 00:01:24 +0100

    run
    run

--- backend/main-service/run.py
+++ backend/main-service/run.py
@@ -1,7 +1,12 @@
-from app import create_app
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+from app import create_app, socketio
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
+
 
